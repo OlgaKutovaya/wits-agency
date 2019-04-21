1b5eef4acb7f0d57802dfeb358600905178cd74a
OlgaKutovaya <o.kutovaya.ak@gmail.com>
2019-04-21 21:23:10 +0300

    header on scroll was changed

--- js/index.js
+++ js/index.js
@@ -5,6 +5,15 @@ $(document).ready(function () {
       $content = $('#content');
    let timeFillPath = 3500;
 
+   let $informativePage = $('#informativePage'),
+      $header = $('#header'),
+      $logo = $('#logo'),
+      $langInfo = $('.lang-info'),
+      $notifications = $('.notifications'),
+      $ringImg = $('#ring-img'),
+      $counterNotification = $('.count-notifications'),
+      $dropDown = $('.dropdown');
+
    // Fill svg image
    setTimeout(function () {
       $svgPath.addClass('fill-path');
@@ -31,5 +40,29 @@ $(document).ready(function () {
       }
    });
 
+   // change header depending on offset of the #informativePage element in relation to viewport
+   $(window).scroll(function () {
+      let informativePageOffset = $informativePage.offset(),
+         scrollTop = $(document).scrollTop(),
+         headerHeight = $header.height();
+
+      if (informativePageOffset.top < scrollTop + headerHeight) {
+         $header.addClass('bg-transparent').removeClass('bg-cod-gray');
+         $logo.attr('src', 'img/black-logo.svg');
+         $langInfo.addClass('color-cod-gray');
+         $notifications.addClass('color-cod-gray');
+         $ringImg.attr('src', 'img/black-ring.svg');
+         $counterNotification.addClass('d-none');
+         $dropDown.addClass('d-block').removeClass('d-none')
+      } else {
+         $header.addClass('bg-cod-gray').removeClass('bg-transparent');
+         $logo.attr('src', 'img/logo.svg');
+         $langInfo.removeClass('color-cod-gray');
+         $notifications.removeClass('color-cod-gray');
+         $ringImg.attr('src', 'img/ring.svg');
+         $counterNotification.removeClass('d-none');
+         $dropDown.addClass('d-none').removeClass('d-block')
+      }
+   });
 
 });
